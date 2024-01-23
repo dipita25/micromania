452ac31c7fc06197de3857b028fa84187403d00e
jonathan Dipita <jonathandipita@gmail.com>
2024-01-23 11:19:01 +0400

    add model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_23_070649) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_23_071839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_23_070649) do
     t.string "name"
     t.float "price"
     t.string "platform"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "phone"
+    t.string "adress"
+    t.boolean "isOwner"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
