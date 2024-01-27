dbc60cde54bdad0fa3655d644b853c18bf9cdc4b
barbedaren <darenbarbe.jedi@gmail.com>
2024-01-27 14:42:15 +0400

    booking controlleur

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,15 @@ Rails.application.routes.draw do
   get "games/:id/edit", to: "games#edit", as: :edit_game
   patch "games/:id", to: "games#update", as: :update_game
   delete "games/:id", to: "games#destroy", as: :destroy_game
+
+
+
+
+  get "bookings/:id", to: "bookings#show", as: :booking
+  get "bookings/new", to: "bookings#new", as: :new_booking
+  post "bookings/", to: "bookings#create", as: :create_booking
+  get "bookings/:id/edit", to: "bookings#edit", as: :edit_booking
+  patch "bookings/:id", to: "bookings#update", as: :update_booking
+  delete "bookings/:id", to: "bookings#destroy", as: :destroy_booking
+
 end
