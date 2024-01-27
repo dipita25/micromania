01687460ecd341eec6bfe1c7c6df99497235f725
jonathan Dipita <jonathandipita@gmail.com>
2024-01-27 14:15:15 +0400

    adding photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root; to: "games#index"
-
-  get "games", to: "games#index"
+  root "games#index"
   get "games/new", to: "games#new", as: :new_game
   get "games/:id", to: "games#show", as: :game
-  post "games/", to: "games#create", as: :create_game
+  post "games/", to: "games#create"
   get "games/:id/edit", to: "games#edit", as: :edit_game
   patch "games/:id", to: "games#update", as: :update_game
   delete "games/:id", to: "games#destroy", as: :destroy_game
